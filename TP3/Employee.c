4ea08c50cde61f43d060659b03ea5f713519dd98
NicolasArak <nicolasarakaki@users.noreply.github.com>
2019-11-05 14:24:07 -0300

    Agrego el archivo data2.csv, que contiene los datos guardados en binario

--- TP3/Employee.c
+++ TP3/Employee.c
@@ -230,14 +230,14 @@ int menuConOpcion(int reintentos, int numOpcMin, int numOpcMax)
 
     printf("\n -*- MENU DE OPCIONES -*-\n\n ");
     printf(" 1. Cargar los datos de los empleados desde el archivo data.csv (modo texto)\n ");
-    printf(" 2. Cargar los datos de los empleados desde el archivo data.bin (modo binario)\n ");
+    printf(" 2. Cargar los datos de los empleados desde el archivo data2.csv (modo binario)\n ");
     printf(" 3. Alta de empleado\n ");
     printf(" 4. Modificar datos de empleado\n ");
     printf(" 5. Baja de empleado\n ");
     printf(" 6. Listar empleados\n ");
     printf(" 7. Ordenar empleados\n ");
     printf(" 8. Guardar los datos de los empleados en el archivo data.csv (modo texto)\n ");
-    printf(" 9. Guardar los datos de los empleados en el archivo data.bin (modo binario)\n ");
+    printf(" 9. Guardar los datos de los empleados en el archivo data2.csv (modo binario)\n ");
     printf(" 10. Salir\n ");
 
     fflush(stdin);
--- TP3/main.c
+++ TP3/main.c
@@ -9,14 +9,14 @@
 /****************************************************
     Menu:
      1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).
-     2. Cargar los datos de los empleados desde el archivo data.bin (modo binario).
+     2. Cargar los datos de los empleados desde el archivo data2.csv (modo binario).
      3. Alta de empleado
      4. Modificar datos de empleado
      5. Baja de empleado
      6. Listar empleados
      7. Ordenar empleados
      8. Guardar los datos de los empleados en el archivo data.csv (modo texto).
-     9. Guardar los datos de los empleados en el archivo data.bin (modo binario).
+     9. Guardar los datos de los empleados en el archivo data2.csv (modo binario).
     10. Salir
 *****************************************************/
 
@@ -132,7 +132,7 @@ int main()
                 break;
 
             case 8:
-                if(controller_saveAsText("data3.csv", listaEmpleados)==TODOOK)
+                if(controller_saveAsText("data.csv", listaEmpleados)==TODOOK)
                     printf("\n\nDatos guardados exitosamente...\n");
                 break;
 
